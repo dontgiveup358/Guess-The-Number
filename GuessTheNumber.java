491de4eaf45666fae5de13cbc08033f917777323
Sarp <sarpgiousouf@gmail.com>
2024-07-11 18:43:12 +0300

    Add files via upload

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -10,29 +10,27 @@ Random random = new Random();
 int count=0;
 int randomNumber = random.nextInt(1,51);
 
-while(true){
+while(true) {
     count++;
-    System.out.println("Guess a number");
+    System.out.println("Guess a number between 1 and 50");
     int num1 = scanner.nextInt();
 
-    if(num1>randomNumber){
-        System.out.println("Too big! Try again");
+    if (num1 > 0 & num1 < 51) {
+
+        if (num1 > randomNumber) {
+            System.out.println("Too big! Try again.");
+
+        } else if (num1 < randomNumber) {
+            System.out.println("Too small! Try again");
+        } else if (num1 == randomNumber) {
+            System.out.println("Congratulations, you guessed the number!");
+            System.out.println("Number of tries" + " " + count);
+            break;
+        }
+        } else {
+            System.out.println("Not a valid number for this game");
+        }
     }
-    else if(num1<randomNumber){
-        System.out.println("Too small! Try again");
-    }
-    else{
-        System.out.println("Congratulations, you guessed the number!");
-        System.out.println("Number of tries"+" "+ count);
-        break;
-    }
-}
-
-
-
-
-
-
 
     }
 }
